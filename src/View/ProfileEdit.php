bc5ea96c47eb7e0adc44bce1441c7741ea759d88
Drindrin77 <ji.leanna@outlook.com>
2020-04-16 12:29:09 +0200

    move script to Script/ProfileEdit.js

--- src/View/ProfileEdit.php
+++ src/View/ProfileEdit.php
@@ -1,4 +1,3 @@
-
 <div class="form-group">
     <label for="passwordInput">Ancien mot de passe</label>
     <br>
@@ -19,60 +18,3 @@
 <div>
     <input type="button" id="btnSubmit" value="Confirmer"/>
 </div>
-
-
-<script>
-
-
-        $("#btnSubmit").click(function(e){
-            let oldPassword = $("#oldPassword").val()
-            let newPassword = $("#newPassword").val()
-            let confirmPassword = $("#confirmPassword").val()
-
-            let isValid = true
-            if(!oldPassword.trim()){
-                console.log("OLD PASSWORD EMPTY")
-                isValid = false
-            }
-            if(!newPassword.trim()){
-                console.log("NEW PASSWORD EMPTY")
-                isValid = false
-            }
-            if(!confirmPassword.trim()){
-                console.log("CONFIRM PASSWORD EMPTY")
-                isValid = false
-            }
-            if(newPassword != confirmPassword){
-                console.log("NEWPASSWORD != CONFIRMPASSWORD")
-                isValid = false
-            }
-
-            if(isValid){
-                if(newPassword == oldPassword){
-                    console.log("NEWPASSWORD == OLDPASSWORd")
-                }else{
-
-                    $.post("/ajax.php?entity=user&action=resetpassword",
-                    {   
-                        'oldPassword': oldPassword,
-                        'newPassword': newPassword
-                    })
-                    .fail(function(e){
-                        console.log("fail",e)
-                    })
-                    .done(function(e){
-                        console.log("done",e)
-                        if(e['status']==='success'){
-                            console.log("success")
-                        }else{
-                            console.log("error")
-                        }
-                    })
-
-                }
-            }
-            
-        })
-
-
-</script>
