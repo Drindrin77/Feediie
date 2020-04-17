d0e7141a25d0f30d24f40fde952dc5a54e721d85
Drindrin77 <ji.leanna@outlook.com>
2020-04-17 14:58:49 +0200

    update view

--- src/View/Layout/header.php
+++ src/View/Layout/header.php
@@ -1,3 +1,36 @@
-<div>
-    header
-</div>
+<nav class="navbar navbar-expand-lg" style="background-color: orange">
+    <nav class="navbar">
+    <a class="navbar-brand" href="/">
+        <img src="/Images/Icon/logo.png" width="150" height="30" class="d-inline-block align-top" alt="">
+    </a>
+    </nav>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+
+
+    <button type="button" class="btn btn-primary">
+        Chat <span class="badge badge-light">9</span>
+        <span class="sr-only">unread messages</span>
+    </button>
+
+
+    <div class="nav-item dropdown my-2 my-lg-0">
+
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          icon + user name
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/profile/">Voir le profil</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Se déconnecter</a>
+        </div>
+    </div>
+
+  </div>
+</nav>
--- src/View/Pages/ProfileView.php
+++ src/View/Pages/ProfileView.php
@@ -4,66 +4,73 @@
     $hobbies = $this->data['hobbies'];
     $personalities = $this->data['personalities'];
     $favoriteDish = $this->data['favoriteDish'];
-    $photos = $this->data['photos'];
+    //            <p>admin : <?= $infoUser['isadmin']
 ?>
 
 <div>
 
+    <div id="parentTop">
+        <div style="width:200px; height:200px;" id="photos">
+            <?php include_once('UserPhoto.php'); ?>
+        </div>
 
-    <div style="width:200px; height:200px" id="photos">
-        <?php include_once('CarouselPhoto.php'); ?>
+        <div id="info">
+            <p>Nom : <?= $infoUser['lastname']?></p>
+            <p>Prenom : <?= $infoUser['firstname']?></p>
+            <p>Age : <?= $infoUser['age']?></p>
+            <p>description : <?= $infoUser['description']?></p>
+            <p>sexe : <?= $infoUser['sex']?></p>
+            <p>ville : <?= $infoUser['city'].' ('. $infoUser['zipcode']. ')' ?> </p>
+        </div>
+
+        <?php 
+            if($isCurrentUser){
+                ?>
+                    <div id="edit"><button onclick="location.href='/profile/edit'">Editer le profil </button></div>
+                <?php
+            }
+        ?>
     </div>
 
-
-    <?php 
-        if($isCurrentUser){
-            echo '<button>Editer le profil </button>';
-        }
-    ?>
-
-    <div id="info">
-        <p>Nom : <?= $infoUser['lastname']?></p>
-        <p>Prenom : <?= $infoUser['firstname']?></p>
-        <p>Age : <?= $infoUser['age']?></p>
-        <p>description : <?= $infoUser['description']?></p>
-        <p>sexe : <?= $infoUser['sex']?></p>
-        <p>ville : <?= $infoUser['city'].' ('. $infoUser['zipcode']. ')' ?> </p>
-        <p>admin : <?= $infoUser['isadmin']?></p>
-    </div>
-
-    <div id="hobbies">
-        <h3> Mes hobby </h3>
-        <?php foreach($hobbies as $hobby): ?>
-            <div style="width: 150px" class="alert alert-primary" role="alert">
-                <?= $hobby['name'] ?>
-            </div>
-        <?php endforeach ?>
-    </div>
-
-
-    <div id="personalities">
-        <h3> Ma personnalité </h3>
-        <?php foreach($personalities as $personality): ?>
-            <div class="card" style="width: 150px;">
-                <img src=<?=$personality['iconurl']?> class="card-img-top" alt="...">
-                <h5 class="card-title"><?= $personality['name']?></h5>
-                <div class="card-body">
-                    <p class="card-text"><?= $personality['description']?></p>
+    <?php if(!empty($hobbies)) {?>
+        <div id="hobbies">
+            <h3> Mes hobby </h3>
+            <?php foreach($hobbies as $hobby): ?>
+                <div style="width: 150px" class="alert alert-primary" role="alert">
+                    <?= $hobby['name'] ?>
                 </div>
-            </div>
-        <?php endforeach ?>
-    </div>
+            <?php endforeach ?>
+        </div>
+    <?php } ?>
 
-    <div id="likeeat">
-        <h3> Ce que j'aime manger </h3>
-        <?php foreach($favoriteDish as $favorite): ?>
-            <div class="card" style="width: 150px;">
-                <img src=<?=$favorite['iconurl']?> class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text"><?= $favorite['name']?></p>
+    <?php if(!empty($personalities)) {?>
+        <div id="personalities">
+            <h3> Ma personnalité </h3>
+            <?php foreach($personalities as $personality): ?>
+                <div class="card" style="width: 150px;">
+                    <img src=<?=$personality['iconurl']?> class="card-img-top" alt="...">
+                    <h5 class="card-title"><?= $personality['name']?></h5>
+                    <div class="card-body">
+                        <p class="card-text"><?= $personality['description']?></p>
+                    </div>
                 </div>
-            </div>
-        <?php endforeach ?>
-    </div>
+            <?php endforeach ?>
+        </div>
+    <?php } ?>
+
+    <?php if(!empty($favoriteDish)) {?>
+
+        <div id="likeeat">
+            <h3> Ce que j'aime manger </h3>
+            <?php foreach($favoriteDish as $favorite): ?>
+                <div class="card" style="width: 150px;">
+                    <img src=<?=$favorite['iconurl']?> class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text"><?= $favorite['name']?></p>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+    <?php } ?>
 
 </div>
