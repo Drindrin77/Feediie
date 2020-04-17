c7b5841b3437070d79346ec9db7200328b7c65a9
Drindrin77 <ji.leanna@outlook.com>
2020-04-17 09:45:40 +0200

    skeleton images repository

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ if(session_status() == PHP_SESSION_NONE)
     session_start();
 
 define('CONST_INCLUDE', NULL);
+define('PATH_USER_PHOTO', '/Images/UserUpload');
+define('PATH_DISH_PHOTO', '/Images/Dish');
+define('PATH_ICON_PHOTO', '/Images/Icon');
 
 require_once("../src/Service/DBConnection.php");
 require_once("../src/Service/AuthService.php");
