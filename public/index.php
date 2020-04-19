4455fa2f8c3d84bdb917fc34a0cf2f4517bd121d
Drindrin77 <ji.leanna@outlook.com>
2020-04-19 11:54:34 +0200

    add constant path

--- public/index.php
+++ public/index.php
@@ -4,10 +4,11 @@ if(session_status() == PHP_SESSION_NONE)
     session_start();
 
 define('CONST_INCLUDE', NULL);
-define('PATH_USER_PHOTO', '/Images/UserUpload');
-define('PATH_DISH_PHOTO', '/Images/Dish');
-define('PATH_ICON_PHOTO', '/Images/Icon');
+define('PATH_USER_PHOTO', '/Images/UserUpload/');
+define('PATH_DISH_PHOTO', '/Images/Dish/');
+define('PATH_ICON_PHOTO', '/Images/Icon/');
 define('PATH_DEFAULT_USER_PHOTO', '/Images/UserUpload/default.png');
+define('MAX_USER_PHOTO',10);
 
 require_once("../src/Service/DBConnection.php");
 require_once("../src/Service/AuthService.php");
@@ -50,9 +51,6 @@ if(empty($routes[1]) || file_exists($controllerPath)){
         $action = null;
     }
     $viewModel = $controller->execute($action); //return ViewModel 
-    if(AuthService::isAuthenticated()){
-        $viewModel->setHeaderInfo();
-    }
 }
 else{
     $viewModel = new ViewModel('Error404');
