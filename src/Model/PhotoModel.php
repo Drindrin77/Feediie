d2ed721503cf13cff99fe6f812e5730416e77a5b
Drindrin77 <ji.leanna@outlook.com>
2020-04-19 11:55:06 +0200

    add functions Model

--- src/Model/PhotoModel.php
+++ src/Model/PhotoModel.php
@@ -10,10 +10,21 @@ class PhotoModel extends DBConnection{
         return $req->fetchAll();
     }
 
-    public static function getFirstPhoto($idUser){
-        $req = self::$pdo->prepare("select * from photo where iduser = ? and priority=1");
+    public static function getPriorityPhoto($idUser){
+        $req = self::$pdo->prepare("select * from photo where iduser = ? and priority=true");
         $req->execute(array($idUser));
         return $req->fetch();
+    }
+
+    public static function addPhoto($idUser, $path, $priority=false){
+        $req = self::$pdo->prepare("insert into photo values(default, ?, ?, ?)");
+        $priority = $priority ? 'true':'false';
+        return $req->execute(array($path,$priority,$idUser));
+    }
+
+    public static function deletePhoto($url){
+        $req = self::$pdo->prepare("delete from photo where url=?");
+        return $req->execute(array($url));
     }
 
 }
