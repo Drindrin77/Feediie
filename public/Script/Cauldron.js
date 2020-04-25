404fa87f9ca09bb818392221c839a75f28d151d5
BastianPadiglione <bastianpadiglione@numericable.fr>
2020-04-25 23:10:44 +0200

    gestion dynamique de la taille de a chatbox et de la liste des matchs

--- public/Script/Cauldron.js
+++ public/Script/Cauldron.js
@@ -1,1 +1,35 @@
-$(window).load()
+function spaceBelow2Div(divA, divB) {
+    const firstDiv=$(divA).offset();
+    const firstDivHeight = $(divA).height();
+    const secondDiv=$(divB).offset();
+    console.log(firstDiv);
+    const spaceBelow = secondDiv.top-firstDiv.top;
+
+    return spaceBelow
+}
+
+function setMatchedUserContainerHeight(){
+    const height = spaceBelow2Div("#pageContainer", "#footer");
+    $("#matchedUserContainer").height(height );
+    console.log(height);
+}
+
+function setChatBoxSize(){
+    let height = spaceBelow2Div("#pageContainer", "#footer");
+    console.log(height);
+
+    height -= ($("#userMessageArea").height() + $("#chatSelectedContact").height());
+    console.log(height);
+
+    $("#chatBox").height(height);
+}
+
+$(document).ready(function() {
+    setMatchedUserContainerHeight();
+    setChatBoxSize();
+});
+
+$(window).resize(function(){
+    setMatchedUserContainerHeight();
+    setChatBoxSize();
+});
