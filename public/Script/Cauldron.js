da711892c7614938523a9148b6e230f96f855d64
BastianPadiglione <bastianpadiglione@numericable.fr>
2020-04-26 19:17:03 +0200

    selection js de l'utilisateur à qui parler

--- public/Script/Cauldron.js
+++ public/Script/Cauldron.js
@@ -2,7 +2,6 @@ function spaceBelow2Div(divA, divB) {
     const firstDiv=$(divA).offset();
     const firstDivHeight = $(divA).height();
     const secondDiv=$(divB).offset();
-    console.log(firstDiv);
     const spaceBelow = secondDiv.top-firstDiv.top;
 
     return spaceBelow
@@ -11,15 +10,12 @@ function spaceBelow2Div(divA, divB) {
 function setMatchedUserContainerHeight(){
     const height = spaceBelow2Div("#pageContainer", "#footer");
     $("#matchedUserContainer").height(height );
-    console.log(height);
 }
 
 function setChatBoxSize(){
     let height = spaceBelow2Div("#pageContainer", "#footer");
-    console.log(height);
 
     height -= ($("#userMessageArea").height() + $("#chatSelectedContact").height());
-    console.log(height);
 
     $("#chatBox").height(height);
 }
@@ -32,4 +28,28 @@ $(document).ready(function() {
 $(window).resize(function(){
     setMatchedUserContainerHeight();
     setChatBoxSize();
+});
+
+//selectionne un utilisateur dans la liste des matchs
+$("#matchedUserContainer").on("click", ".matchedUser", function (event) {
+    const uniqId = $(event.currentTarget).attr("data-uniqid");
+    const photo = $("#photo-" + uniqId).attr("src");
+    const photoDesciption = $("#photo-" + uniqId).attr("alt");
+    const name = $("#name-" + uniqId).text();
+
+    console.log(event.currentTarget);
+    $(".selectedMatchedUser").addClass("matchedUser");
+    $(".selectedMatchedUser").removeClass("selectedMatchedUser");
+
+    $("#user-" + uniqId).addClass("selectedMatchedUser")
+    $("#user-" + uniqId).removeClass("matchedUser")
+    console.log(event.currentTarget);
+
+    $("#chatSelectedContact").attr("data-uniqid", uniqId);
+    $("#chatSelectedContact").attr("href", "/profile/" + uniqId);
+    $("#selectedContactPhoto").attr("src", photo);
+    $("#selectedContactPhoto").attr("alt", photoDesciption);
+    $("#selectedContactName").text(name);
+
+    setChatBoxSize(); //TODO enlever dès que les images sont de taille fixe
 });
--- src/View/Pages/Cauldron.php
+++ src/View/Pages/Cauldron.php
@@ -7,17 +7,21 @@ $usersMatchedArray = $this->data["usersMatched"];
         <div id="matchedUserContainer" class="col-md-3">
             <div class="container-fluid">
                 <?php
+                $isFirst = true;
                 foreach ($usersMatchedArray as $matchedUser) {
                     ?>
-                    <div class="row matchedUser align-items-center" data-uniqId="<?php echo $matchedUser['uniq_id'] ?>">
-                        <img class="col-md-3 rounded-circle" src="<?php echo $matchedUser['photo_url'] ?>"
+                    
+                    
+                    <div id="user-<?php echo $matchedUser['uniq_id'] ?>" class="row <?php echo $isFirst ?  "selectedMatchedUser" : "matchedUser" ?> align-items-center" data-uniqId="<?php echo $matchedUser['uniq_id'] ?>">
+                        <img id="photo-<?php echo $matchedUser['uniq_id'] ?>" class="col-md-3 rounded-circle" src="<?php echo $matchedUser['photo_url'] ?>"
                              alt="Photo de profil de <?php echo $matchedUser["name"] ?>">
-                        <span class="col-md-5"><?php echo $matchedUser["name"] ?></span>
+                        <span id="name-<?php echo $matchedUser['uniq_id'] ?>" class="col-md-5"><?php echo $matchedUser["name"] ?></span>
                         <span class="col-md-4"><?php echo $matchedUser["age"] ?> ans</span>
                         <span class="col-md-12">Match le <?php echo $matchedUser["date_match"] ?> </span>
                     </div>
 
                     <?php
+                    $isFirst = false;
                 }
                 ?>
             </div>
@@ -27,8 +31,8 @@ $usersMatchedArray = $this->data["usersMatched"];
                 <a id="chatSelectedContact" class=" row align-items-center"
                    href="/profile/<?php echo $usersMatchedArray[0]['uniq_id'] ?>"
                    data-uniqId="<?php echo $usersMatchedArray[0]['uniq_id'] ?>">
-                    <img class="col-2 rounded-circle" src="<?php echo $usersMatchedArray[0]['photo_url'] ?>">
-                    <span class="col-8 offset-1"><?php echo $usersMatchedArray[0]["name"] ?></span>
+                    <img id="selectedContactPhoto" class="col-2 rounded-circle" src="<?php echo $usersMatchedArray[0]['photo_url'] ?>">
+                    <span id="selectedContactName" class="col-8 offset-1"><?php echo $usersMatchedArray[0]["name"] ?></span>
                 </a>
                 <div id="chatBox" class="row"></div>
                 <div id="userMessageArea" class="row ">
