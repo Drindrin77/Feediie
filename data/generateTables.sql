e943c324764b47f1b6c77ce10f61efe2df106dfd
Drindrin77 <ji.leanna@outlook.com>
2020-04-30 11:41:04 +0200

    modify relation type

--- data/generateTables.sql
+++ data/generateTables.sql
@@ -7,10 +7,10 @@
 ------------------------------------------------------------
 -- Table: Category
 ------------------------------------------------------------
-CREATE TABLE Category(
-	idCategory   SERIAL NOT NULL ,
+CREATE TABLE RelationType(
+	idRelationType   SERIAL NOT NULL ,
 	nom          VARCHAR (128) NOT NULL  ,
-	CONSTRAINT Category_PK PRIMARY KEY (idCategory)
+	CONSTRAINT RelationType_PK PRIMARY KEY (idRelationType)
 )WITHOUT OIDS;
 
 
@@ -92,13 +92,13 @@ CREATE TABLE likedUser(
 ------------------------------------------------------------
 -- Table: interestedCategory
 ------------------------------------------------------------
-CREATE TABLE interestedCategory(
+CREATE TABLE interestedRelationType(
 	idUser       INT  NOT NULL ,
-	idCategory   INT  NOT NULL  ,
-	CONSTRAINT interestedCategory_PK PRIMARY KEY (idUser,idCategory)
+	idRelationType   INT  NOT NULL  ,
+	CONSTRAINT interestedRelationType_PK PRIMARY KEY (idUser,idRelationType)
 
 	,CONSTRAINT interestedCategory_User_FK FOREIGN KEY (idUser) REFERENCES FeediieUser(idUser)
-	,CONSTRAINT interestedCategory_Category0_FK FOREIGN KEY (idCategory) REFERENCES Category(idCategory)
+	,CONSTRAINT interestedRelationType_FK FOREIGN KEY (idRelationType) REFERENCES RelationType(idRelationType)
 )WITHOUT OIDS;
 
 
@@ -157,15 +157,6 @@ CREATE TABLE idea(
 	,CONSTRAINT idea_User_FK FOREIGN KEY (idUser) REFERENCES FeediieUser(idUser)
 )WITHOUT OIDS;
 
-------------------------------------------------------------
--- Table: TypeRestaurant
-------------------------------------------------------------
-CREATE TABLE TypeRestaurant(
-	idRestaurant   SERIAL NOT NULL ,
-	name           VARCHAR (500) NOT NULL  ,
-	CONSTRAINT TypeRestaurant_PK PRIMARY KEY (idRestaurant)
-)WITHOUT OIDS;
-
 
 ------------------------------------------------------------
 -- Table: Dish
@@ -198,19 +189,6 @@ CREATE TABLE Hobby(
 	name      VARCHAR (128) NOT NULL  ,
 	CONSTRAINT Hobby_PK PRIMARY KEY (idHobby)
 )WITHOUT OIDS;
-
-------------------------------------------------------------
--- Table: firstDate
-------------------------------------------------------------
-CREATE TABLE firstDate(
-	idUser         INT  NOT NULL ,
-	idRestaurant   INT  NOT NULL  ,
-	CONSTRAINT firstDate_PK PRIMARY KEY (idUser,idRestaurant)
-
-	,CONSTRAINT firstDate__User_FK FOREIGN KEY (idUser) REFERENCES FeediieUser(idUser)
-	,CONSTRAINT firstDate_TypeRestaurant0_FK FOREIGN KEY (idRestaurant) REFERENCES TypeRestaurant(idRestaurant)
-)WITHOUT OIDS;
-
 
 ------------------------------------------------------------
 -- Table: likeEat
