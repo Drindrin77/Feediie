fd559a255d248719d1e114973a2a5f2544fbd3d5
BastianPadiglione <bastianpadiglione@numericable.fr>
2020-05-02 14:24:17 +0200

    regle le bug creant des erreurs php lorsque qu'un utilisateur n'ayant pas de matchs accède à la page de chat

--- data/insertData.sql
+++ data/insertData.sql
@@ -30,7 +30,7 @@ INSERT INTO FeediieUser VALUES
 
 INSERT INTO FeediieUser VALUES
     (default, 'arjaze98e58a5ab','Valentin', '1998-02-14','valentinbgdu54@gmail.com','$2y$10$DMjYUV76Y7GYJlC45pTOKOG8UjFw.tnEfm1WgIqlLzoiImYEKf4ra', 'Je suis valentin je viens de Nancy',
-        default, default, default , default default,'token2', true, 1, 5, 'Homme');
+        default, default, default , default, default,'token2', true, 1, 5, 'Homme');
 
 INSERT INTO FeediieUser VALUES
     (default, 'brjaze98e58a5ab','Naruto', '1992-01-30','naruto@gmail.com','$2y$10$DMjYUV76Y7GYJlC45pTOKOG8UjFw.tnEfm1WgIqlLzoiImYEKf4ra', 'balbalbal',
@@ -54,7 +54,8 @@ INSERT INTO LikedUser VALUES (3, 2, '2020-04-18 15:35:36', true);
 INSERT INTO LikedUser VALUES (2, 4, '2020-04-18 13:54:36', true);
 INSERT INTO LikedUser VALUES (4, 2, '2020-04-18 13:54:36', true);
 
-INSERT INTO LikedUser VALUES (2, 5, '2020-04-18 13:54:36', false);
+INSERT INTO LikedUser VALUES (2, 5, '2020-04-18 13:54:36', true);
+INSERT INTO LikedUser VALUES (5, 2, '2020-04-18 13:54:36', true);
 
 INSERT INTO LikedUser VALUES (6, 2, '2020-04-18 13:54:36', false);
 
