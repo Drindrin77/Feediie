4ac4af0fb1af3bfe4099d8dabff3bb24600688ea
BastianPadiglione <bastianpadiglione@numericable.fr>
2020-05-05 20:40:17 +0200

    amelioration css chat

--- src/View/Pages/Cauldron.php
+++ src/View/Pages/Cauldron.php
@@ -116,8 +116,8 @@ if (sizeof($usersMatchedArray) === 0) {
                         </div>
                     </div>
                     <div id="userMessageArea" class="row align-items-center">
-                        <textarea id="inputMessage" class="col-10 form-control" maxlength="500" rows="1"></textarea>
-                        <div class="col-2 text-center">
+                        <textarea id="inputMessage" class="col-10 col-md-11 form-control" maxlength="500" rows="1"></textarea>
+                        <div class="col-2 col-md-1 text-center">
                             <button id="sendMessageButton" class="btn btn-primary" type="button"><i
                                         class="fas fa-paper-plane"></i></button>
                         </div>
